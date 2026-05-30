fad21bb8e23fd34523bd1d7f0bbd80591b3bab58
Paulo <yagaichi@gmail.com>
2026-05-30 15:55:54 +0100

    remove: barra de valores removida da pagina

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Hero from '@/components/Hero';
-import ValoresBarra from '@/components/ValoresBarra';
 import SobreNos from '@/components/SobreNos';
 import Produtos from '@/components/Produtos';
 import Localizacao from '@/components/Localizacao';
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <ValoresBarra />
       <SobreNos />
       <Produtos />
       <Localizacao />
